6be10998c384c060d482b9ef4cfa1bd109d72d3a
Guilherme <guilhermebromonschenkel@gmail.com>
2019-07-16 09:49:04 -0300

    Improved auto deploy method

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to anonym!');
+    expect(page.getTitleText()).toEqual('Welcome to anonymous-message-board!');
   });
 
   afterEach(async () => {
