c4381253c388eb16fd5b5b8b4da57e02402e07f3
“Guilherme <guilhermebromonschenkel@gmail.com>
2019-07-26 13:45:46 -0300

    Removed redundant route

--- src/api/routes/ReplyRouter.js
+++ src/api/routes/ReplyRouter.js
@@ -6,7 +6,7 @@ const ReplyController = require('../controllers/ReplyController')
 module.exports = {
     config(app){
         app.post('/replies', ReplyController.createReply)
-        app.get('/replies/:thread_id', ReplyController.getRepliesByThreadId)
+        //app.get('/replies/:thread_id', ReplyController.getRepliesByThreadId)
         app.get('/replies/:board_id', ReplyController.getRepliesByBoardId)
         app.delete('/replies', ReplyController.deleteReply)
         app.put('/replies', ReplyController.updateReply)
